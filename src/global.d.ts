dedf5caed54af10447760982b92fb8a7e6a2fc18
moonbe77 <moonbe77@gmail.com>
2025-01-06 11:48:10 +1100

    feat: add TypeScript declaration file for moon-ui components

---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+declare module "moon-ui" {
+  import { FC } from "react";
+  import { ButtonProps } from "./components/Button";
+  import { TopNavbarProps } from "./components/TopNavbar";
+
+  export const Button: FC<ButtonProps>;
+  export const TopNavbar: FC<TopNavbarProps>;
+}
